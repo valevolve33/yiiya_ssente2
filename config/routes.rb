722cd772c3f1eafcdf8d9982f2c729e10d40ec5a
valevolve33 <valevolve33@gmail.com>
2015-08-05 12:57:43 +0300

    my 2 success

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+ root 'welcome#index'
+ get 'welcome/aboutUs'
+ get 'welcome/contactUs'
+ get 'welcome/payments'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
